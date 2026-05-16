1955ecbe5d70509850af7ddcf931a1ba61bea3d4
Lila <lila.88.xx@gmail.com>
2026-05-16 10:09:02 +0200

    reaktywność

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { useState } from "react";
+
 function App() {
-    let email = 'anna@agh.edu.pl';
+    const [email, setEmail] = useState('anna@agh.edu.pl');
 
     function handleChange(event) {
-        console.log(event.target.value);
+        setEmail(event.target.value);
+    }
+
+    let message;
+
+    if (email.length < 10) {
+        message = <div>Ale masz krótki adres!</div>;
+    } else if (email.length < 15) {
+        message = <div>Twój adres e-mail jest w sam raz.</div>;
+    } else {
+        message = <div>Twój adres e-mail jest stanowczo za długi.</div>;
     }
 
     return (
         <div>
             <h1>System do zapisów na zajęcia</h1>
-            <h2>Twój e-mail to {email}.</h2>
-            <input type="text" onChange={handleChange}/>
+            <h2>Twój e-mail to {email}</h2>
+
+            <input
+                type="text"
+                value={email}
+                onChange={handleChange}
+            />
+
+            {message}
         </div>
     );
 }
+
 export default App;
