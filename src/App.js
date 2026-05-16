53ade1dc3ea3cb12e5886a31592e18418ea4b1b4
Lila <lila.88.xx@gmail.com>
2026-05-16 13:17:24 +0200

    Dekompozycja - LoginForm.js

--- src/App.js
+++ src/App.js
@@ -2,28 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 import {useState} from "react";
 import "milligram";
-
+import LoginForm from "./LoginForm";
 
 
 function App() {
 const [email, setEmail] = useState('anna@agh.edu.pl');
-
 const [isLoggedIn, setIsLoggedIn] = useState(false);
 
 return (
 <div className = "container">
 
 <h1>Witaj w systemie do zapisów na zajęcia</h1>
-Zaloguj się emailem:
-{!isLoggedIn &&<div>
-<input type="text" onChange={(event)=>setEmail(event.target.value)}/>
-
-<button onClick={() => setIsLoggedIn(true)}>Zaloguj się</button>
-</div>}
+{ !isLoggedIn && <LoginForm onLogin={() => {}}/> }
 
 {isLoggedIn && <div>
 <h1>Witaj {email }</h1>
-<button onClick={() => setIsLoggedIn(false)}>Wyloguj</button>
+<a onClick={() => setIsLoggedIn(false)}>Wyloguj</a>
 
 </div>}
 </div>
